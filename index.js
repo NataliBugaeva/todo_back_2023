91eaa1fa844185f98aa7f6cd6133f6b18355b51a
Anton <anton@brightdot.tech>
2023-04-11 13:00:03 +0400

    'some_fix'

--- index.js
+++ index.js
@@ -6,31 +6,22 @@ import bodyParser from "body-parser";
 
 const corsOptions = {
     origin:'*',
-    credentials:true,            //access-control-allow-credentials:true
+    credentials:true,
     optionSuccessStatus:200,
 }
 
-
-
 //порт на котором будет запускаться сервер
 const PORT = 5000;
-//подключаем базу данных
-//const DB_URL = "mongodb+srv://user:user@cluster0.sckxkji.mongodb.net/?retryWrites=true&w=majority"
 
 //вызов функции express создает экземпляр нашего приложения
 const app = express();
 
-
-
-//const todo_db_model = require('./todo_db-model');
 app.use(express.json());
 app.use(cors(corsOptions));
 
-// app.use(express.json());
 app.use('/api', routerTodolist);
 app.use('/api', routerTask);
 app.use(bodyParser.urlencoded({ extended: false }));
-
 
 app.use((req, res, next) => {
     res.append('Access-Control-Allow-Origin', ['*']);
@@ -38,23 +29,6 @@ app.use((req, res, next) => {
     res.append('Access-Control-Allow-Headers', ['*', 'Authorization']);
     next();
 });
-
-
-// async function startApp() {
-//     try {
-//        await mongoose.connect(DB_URL);
-//        mongoose.set('strictQuery', true);
-// //приложение прослушивает 5000 порт, переданная вторым параметром функция запустится только в случае успешного запуска сервера
-//         app.listen(PORT, () => {
-//             console.log(`SERVER STARTED OP PORT ${5000}`)
-//         })
-//     } catch(e) {
-//         console.log(e)
-//     }
-// }
-//
-// startApp();
-
 
 app.listen(PORT, () => {
     console.log(`SERVER STARTED OP PORT ${5000}`)
